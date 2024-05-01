d5395d40fa488c1ed404de993e9f12afea3c2b0d
Felipe Emerson <felipe.calixto@ccc.ufcg.edu.br>
2024-05-01 16:20:18 -0300

    making changes to home layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,8 +8,9 @@ const App = () => {
     return (
         <>
             <header style={{ display: "flex", gap: "50px", justifyContent: "center"}}>
-                <Link to="/"><h2 style={{ fontSize: "30px"}}>Home</h2></Link>
-                <Link to="/otherpage"><h2 style={{ fontSize: "30px"}}>Other Page</h2></Link>
+                <h1 style={{ paddingTop: "20px", paddingBottom: "20px" }}>Fib Calculator</h1>
+                <Link to="/"><h2 style={{ fontSize: "20px"}}>Home</h2></Link>
+                <Link to="/otherpage"><h2 style={{ fontSize: "20px"}}>Other Page</h2></Link>
             </header>
             <Outlet />
         </>
