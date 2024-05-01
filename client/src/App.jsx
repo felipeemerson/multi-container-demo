009c83ef0c2361d127e385981b28431de737def6
Felipe Emerson <felipe.calixto@ccc.ufcg.edu.br>
2024-05-01 17:46:38 -0300

    changed app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
     return (
         <>
             <header style={{ display: "flex", alignItems: "center", flexDirection: "column" }}>
-                <h1>Fib Calculator</h1>
+                <h1>Fib Calculator V2</h1>
                 <div style={{ display: "flex", gap: "50px" }}>
                     <Link to="/"><h2 style={{ fontSize: "24px"}}>Home</h2></Link>
                     <Link to="/otherpage"><h2 style={{ fontSize: "24 px"}}>Other Page</h2></Link>
